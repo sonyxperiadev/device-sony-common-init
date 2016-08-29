oshmoun/android_hardware_sony_init

init: make init a symlink to init_sony

init/Android.mk
@@ -49,3 +49,22 @@ LOCAL_STATIC_LIBRARIES := \
 LOCAL_CLANG := true
 
 include $(BUILD_EXECUTABLE)
+
+root_init      := $(TARGET_ROOT_OUT)/init
+root_init_real := $(TARGET_ROOT_OUT)/init.real
+
+	# If /init is a file and not a symlink then rename it to /init.real
+	# and make /init be a symlink to /sbin/init_sony (which will execute
+	# /init.real, if appropriate.
+$(root_init_real): $(root_init) $(PRODUCT_OUT)/utilities/init_sony $(PRODUCT_OUT)/utilities/toybox
+	cp $(PRODUCT_OUT)/utilities/toybox $(TARGET_ROOT_OUT)/sbin/toybox
+	cp $(PRODUCT_OUT)/utilities/init_sony $(TARGET_ROOT_OUT)/sbin/init_sony
+	$(hide) if [ ! -L $(root_init) ]; then \
+	  echo "/init $(root_init) isn't a symlink"; \
+	  mv $(root_init) $(root_init_real); \
+	  ln -s sbin/init_sony $(root_init); \
+	else \
+	  echo "/init $(root_init) is already a symlink"; \
+	fi
+
+ALL_DEFAULT_INSTALLED_MODULES += $(root_init_real)

sonyxperiadev/device-sony-common-init

init/Android.mk
@@ -50,3 +50,23 @@ LOCAL_STATIC_LIBRARIES := \
 LOCAL_CLANG := true
 
 include $(BUILD_EXECUTABLE)
+
+root_init      := $(TARGET_ROOT_OUT)/init
+root_init_real := $(TARGET_ROOT_OUT)/init.real
+
+	# If /init is a file and not a symlink then rename it to /init.real
+	# and make /init be a symlink to /sbin/init_sony (which will execute
+	# /init.real, if appropriate.
+$(root_init_real): $(root_init) $(PRODUCT_OUT)/utilities/toybox $(PRODUCT_OUT)/utilities/keycheck $(PRODUCT_OUT)/utilities/init_sony
+	cp $(PRODUCT_OUT)/utilities/toybox $(TARGET_ROOT_OUT)/sbin/toybox_init
+	cp $(PRODUCT_OUT)/utilities/keycheck $(TARGET_ROOT_OUT)/sbin/keycheck
+	cp $(PRODUCT_OUT)/utilities/init_sony $(TARGET_ROOT_OUT)/sbin/init_sony
+	$(hide) if [ ! -L $(root_init) ]; then \
+	  echo "/init $(root_init) isn't a symlink"; \
+	  mv $(root_init) $(root_init_real); \
+	  ln -s sbin/init_sony $(root_init); \
+	else \
+	  echo "/init $(root_init) is already a symlink"; \
+	fi
+
+ALL_DEFAULT_INSTALLED_MODULES += $(root_init_real)